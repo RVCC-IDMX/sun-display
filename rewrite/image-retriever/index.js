1b8f94bfab4ab041ee2dbc9c715cd0f17e9dbac2
Nate7611 <natedowneymain@gmail.com>
2025-03-20 04:33:26 -0400

    Remove canvas

--- rewrite/image-retriever/index.js
+++ rewrite/image-retriever/index.js
@@ -1,16 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const cron = require('node-cron');
-const { createCanvas, loadImage } = require('canvas');
 
 const maxImages = 300 // Temp number
 
 const imageInterval = 15 // Minutes between creating new images
-
-const dateRemoveMask = './masks/date-remove-mask.png'; // Used for every image other than wavelength 171
-const aia171Mask = './masks/aia171-mask.png'; // Used for just wavelength 171 images
-
-const imageSize = 2048; //Change if downloading higher or lower res image
 
 const wavelengths = {
     aia171: [],
@@ -18,12 +12,6 @@ const wavelengths = {
     aia211: [],
     aia304: []
 };
-
-const canvas = createCanvas(imageSize, imageSize);
-const context = canvas.getContext('2d');
-
-context.fillStyle = '#000';
-context.fillRect(0, 0, imageSize, imageSize);
 
 async function ensureDirs() {
     try {
@@ -67,8 +55,10 @@ ensureDirs().then(() => {
 
     saveWavelengths();
 
+    fetchImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0171.jpg', 'aia171')
+
     cron.schedule(`*/${imageInterval} * * * *`, () => {
-        createImages();
+        //createImages();
     });
 });
 
@@ -92,40 +82,13 @@ async function fetchWithRetry(url, retries = 3, delay = 5000) {
 
 // Await might not be nessecary but I dont want to pull 4 images from the servers at once
 async function createImages() {
-    await createImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0171.jpg', aia171Mask, 'aia171');
-    await createImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0193.jpg', dateRemoveMask, 'aia193');
-    await createImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0211.jpg', dateRemoveMask, 'aia211');
-    await createImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0304.jpg', dateRemoveMask, 'aia304');
+    await fetchImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0171.jpg', 'aia171')
+    await fetchImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0193.jpg', 'aia193');
+    await fetchImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0211.jpg', 'aia211');
+    await fetchImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0304.jpg', 'aia304');
     saveWavelengths();
 }
 
-async function createImage(imageUrl, maskUrl, wavelength) {
-    let sun = await fetchWithRetry(imageUrl);
-    let mask = await loadImage(maskUrl);
-
-    if (sun && mask) {
-        context.drawImage(sun, 0, 0, imageSize, imageSize);
-        context.drawImage(mask, 0, 0, imageSize, imageSize);
-
-        //Add date
-        let date = new Date();
-        const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        let dateTimeFileName = `${date.getUTCFullYear()}-${(date.getUTCMonth() + 1).toString().padStart(2, '0')}-${date.getUTCDate().toString().padStart(2, '0')}T${date.getUTCHours().toString().padStart(2, '0')}.${date.getUTCMinutes().toString().padStart(2, '0')}.${date.getUTCSeconds().toString().padStart(2, '0')}`;
-
-        context.fillStyle = '#fff';
-        context.font = 'bold 60pt Arial'
-        context.textAlign = 'center';
-        context.fillText(`${month[date.getMonth()]} ${date.getUTCDate()}, ${date.getFullYear()}`, 1024, 2033);
-
-        const buffer = canvas.toBuffer('image/png');
-        const filePath = `./download/${wavelength}/${wavelength}-${dateTimeFileName}.jpg`;
-        fs.writeFileSync(filePath, buffer);
-        console.log(`Created new image for ${wavelength} at ${dateTimeFileName}`);
-    }
-    else {
-        return;
-    }
-};
 
 // Probably too slow with thousands of images, who knows
 function saveWavelengths() {
@@ -154,12 +117,25 @@ function saveWavelengths() {
     console.log('Updated wavelengths.json');
 };
 
-
-/*
-async function fetchImage(url, path) {
+async function fetchImage(url, wavelength) {
     const response = await fetch(url);
+    const formattedDate = getConvertedDate(response.headers.get('last-modified'));
+
+    console.log(formattedDate);
     const arrayBuffer = await response.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer, 'binary');
-    fs.writeFileSync(path + '/latest.jpg', buffer);
+    fs.writeFileSync(`./download/${wavelength}/` + `${wavelength}-${formattedDate}.jpg`, buffer);
 }
-*/
+
+function getConvertedDate(dateString) {
+
+    const date = new Date(dateString);
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const hours = String(date.getUTCHours()).padStart(2, '0');
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day}T${hours}.${minutes}.${seconds}`;
+}
