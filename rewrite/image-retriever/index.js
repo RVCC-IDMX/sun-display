fbc24df2484bfcebb9e3795c2e48becedab1cf31
Nate7611 <natedowneymain@gmail.com>
2025-03-19 21:28:16 -0400

    Improve error handling

--- rewrite/image-retriever/index.js
+++ rewrite/image-retriever/index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const cron = require('node-cron');
 const { createCanvas, loadImage } = require('canvas');
 
+const imageInterval = 15 // Minutes between creating new images
+
 const dateRemoveMask = './masks/date-remove-mask.png'; // Used for every image other than wavelength 171
 const aia171Mask = './masks/aia171-mask.png'; // Used for just wavelength 171 images
 
@@ -12,14 +14,10 @@ createDir('download/aia193');
 createDir('download/aia211');
 createDir('download/aia304');
 
-/*
-cron.schedule('1 * * * * *', () => {
-    createImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0171.jpg', aia171Mask, 'aia171');
-    createImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0193.jpg', dateRemoveMask, 'aia193');
-    createImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0211.jpg', dateRemoveMask, 'aia211');
-    createImage('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_2048_0304.jpg', dateRemoveMask, 'aia304');
+
+cron.schedule(`* */${imageInterval} * * * *`, () => {
+    createImages();
 });
-*/
 
 createImages();
 
@@ -50,26 +48,38 @@ context.fillStyle = '#000';
 context.fillRect(0, 0, imageSize, imageSize);
 
 async function createImage(imageUrl, maskUrl, wavelength) {
-    let sun = await loadImage(imageUrl);
-    let mask = await loadImage(maskUrl);
+    let sun = await loadImage(imageUrl).catch((err) => {
+        console.error(`Failed to load sun image for ${wavelength}:`, err);
+        return null;
+    });
 
-    context.drawImage(sun, 0, 0, imageSize, imageSize);
-    context.drawImage(mask, 0, 0, imageSize, imageSize);
+    let mask = await loadImage(maskUrl).catch((err) => {
+        console.error(`Failed to load mask:`, err);
+        return null;
+    });
 
-    //Add date
-    let date = new Date();
-    const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let dateTimeFileName = `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()}T${date.getUTCHours()}.${date.getUTCMinutes()}.${date.getUTCSeconds()}`;
+    if (sun && mask) {
+        context.drawImage(sun, 0, 0, imageSize, imageSize);
+        context.drawImage(mask, 0, 0, imageSize, imageSize);
 
-    context.fillStyle = '#fff'
-    context.font = 'bold 60pt Arial'
-    context.textAlign = 'center';
-    context.fillText(`${month[date.getMonth()]} ${date.getUTCDate()}, ${date.getFullYear()}`, 1024, 2033)
+        //Add date
+        let date = new Date();
+        const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let dateTimeFileName = `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()}T${date.getUTCHours()}.${date.getUTCMinutes()}.${date.getUTCSeconds()}`;
 
-    const buffer = canvas.toBuffer('image/png');
-    fs.writeFileSync(`./download/${wavelength}/${wavelength}-${dateTimeFileName}.jpg`, buffer);
-    console.log(`Created new image for ${wavelength} at ${dateTimeFileName}`);
-}
+        context.fillStyle = '#fff';
+        context.font = 'bold 60pt Arial'
+        context.textAlign = 'center';
+        context.fillText(`${month[date.getMonth()]} ${date.getUTCDate()}, ${date.getFullYear()}`, 1024, 2033);
+
+        const buffer = canvas.toBuffer('image/png');
+        fs.writeFileSync(`./download/${wavelength}/${wavelength}-${dateTimeFileName}.jpg`, buffer);
+        console.log(`Created new image for ${wavelength} at ${dateTimeFileName}`);
+    }
+    else {
+        return;
+    }
+};
 
 /*
 async function fetchImage(url, path) {
