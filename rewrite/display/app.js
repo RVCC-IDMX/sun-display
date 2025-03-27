6cb23b772276fc8f1204b5214d92ea082de7674e
Nate7611 <natedowneymain@gmail.com>
2025-03-27 13:07:28 -0400

    Rewrite display to make more performant and cleaner

--- rewrite/display/app.js
+++ rewrite/display/app.js
@@ -5,135 +5,148 @@ const dateText = document.getElementById('date-text');
 const defaultWavelength = 'aia171';
 const pathToRetriever = '../image-retriever/';
 
-const imageToSkip = 5; // How many images giving an input will skip
-
-const imageUpdateDelay = 600000; // 600000 ms = 10 minutes (Time between grabbing new image paths)
-const resetDisplayDelay = 180000; // 180000 ms = 3 minutes (Time between reseting the display to most recent image)
+const imageToSkip = 5; // Number of images to skip when using keyboard, does nothing with mousemove
+const imageUpdateDelay = 600000; // Delay between updating json, 600000 is 10 minutes in ms 
+const resetDisplayDelay = 180000; // Delay before setting current image to latest, 180000 is 3 minutes in ms 
 
 let imgFilePaths = {};
 let imageIndex = 0;
-
-const img = new Image();
-
 let currentWavelength = defaultWavelength;
-
-let resetDisplayIntervalId;
-
-main();
+let resetDisplayIntervalId = null;
 
 async function main() {
     await getFilePaths();
-
     resetDisplay();
+    changeImage();
 
-    updateCanvasImage(imgFilePaths[currentWavelength][imageIndex]);
-    formatDate(imgFilePaths[currentWavelength][imageIndex]);
-
-    document.addEventListener('keydown', (event) => {
-        resetDisplay(); // Resets reset countdown
-        if (event.key === '1') changeWavelength('aia171');
-        if (event.key === '2') changeWavelength('aia193');
-        if (event.key === '3') changeWavelength('aia211');
-        if (event.key === '4') changeWavelength('aia304');
-        if (event.key === 'ArrowLeft') moveBackImage(1);
-        if (event.key === 'ArrowRight') moveForwardImage(1);
-        if (event.key === 'a') moveBackImage(2);
-        if (event.key === 'd') moveForwardImage(2);
-    });
-
-    document.addEventListener('mousemove', (event => {
-        resetDisplay();
-
-        let norm = (event.clientX / window.innerWidth);
-            norm = 1 - norm;
-            norm *= imgFilePaths[currentWavelength].length-1;
-        imageIndex = Math.floor(norm);
-        changeImage();
-    }))
+    // Change throttle time to make it more smooth but if you go too low it will start glitching
+    document.addEventListener('keydown', throttle(handleKeyDown, 100)); // If we dont throttle this as well you can break the program if you spam switch the wavelength
+    document.addEventListener('mousemove', throttle(handleMouseMove, 50));
 }
 
-async function updateCanvasImage(imagePath) {
-    img.src = pathToRetriever + imagePath;
+function handleKeyDown(event) {
+    resetDisplay(); // Reset idle timeout
 
-    img.onload = () => {
-        drawCanvasImage();
+    switch (event.key) {
+        case '1':
+            changeWavelength('aia171');
+            break;
+        case '2':
+            changeWavelength('aia193');
+            break;
+        case '3':
+            changeWavelength('aia211');
+            break;
+        case '4':
+            changeWavelength('aia304');
+            break;
+        case 'ArrowLeft':
+            moveBackImage(1);
+            break;
+        case 'ArrowRight':
+            moveForwardImage(1);
+            break;
+        case 'a':
+            moveBackImage(2);
+            break;
+        case 'd':
+            moveForwardImage(2);
+            break;
+    }
+}
+
+function handleMouseMove(event) {
+    resetDisplay(); // Reset idle timeout
+
+    let norm = (event.clientX / window.innerWidth);
+    norm = 1 - norm;
+    norm *= imgFilePaths[currentWavelength].length - 1;
+    imageIndex = Math.floor(norm);
+    changeImage();
+}
+
+// I have found that this is needed, the awaits stack up and things jump around a lot without throttling the mousemove event
+function throttle(func, limit) {
+    let inThrottle;
+    return function () {
+        if (!inThrottle) {
+            func.apply(this, arguments);
+            inThrottle = true;
+            setTimeout(() => (inThrottle = false), limit);
+        }
     };
 }
 
-async function drawCanvasImage() {
-    let imgBitmap = await createImageBitmap(img);
+async function updateCanvasImage(imagePath) {
+    try {
+        const response = await fetch(pathToRetriever + imagePath);
+        if (!response.ok) {
+            throw new Error(`Error fetching image: ${response.status}`);
+        }
 
-    const maxCanvasWidth = window.innerWidth * 1;
-    const maxCanvasHeight = window.innerHeight * 1;
+        // Using blobs with imageBitmap seems to speed things up
+        const blob = await response.blob();
+        const imgBitmap = await createImageBitmap(blob);
 
-    let scale = Math.min(maxCanvasWidth / img.width, maxCanvasHeight / img.height);
+        const maxCanvasWidth = window.innerWidth;
+        const maxCanvasHeight = window.innerHeight;
+        const scale = Math.min(maxCanvasWidth / imgBitmap.width, maxCanvasHeight / imgBitmap.height);
 
-    canvas.width = img.width * scale;
-    canvas.height = img.height * scale;
+        canvas.width = imgBitmap.width * scale;
+        canvas.height = imgBitmap.height * scale;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(imgBitmap, 0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(imgBitmap, 0, 0, canvas.width, canvas.height);
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 function changeImage() {
-    updateCanvasImage(imgFilePaths[currentWavelength][imageIndex]);
-    formatDate(imgFilePaths[currentWavelength][imageIndex]);
+    const imagePath = imgFilePaths[currentWavelength][imageIndex];
+    if (imagePath) {
+        updateCanvasImage(imagePath);
+        formatDate(imagePath);
+    }
 }
 
 function changeWavelength(wavelength) {
-    if (imgFilePaths[wavelength][imageIndex]) {
+    if (imgFilePaths[wavelength] && imgFilePaths[wavelength][imageIndex]) {
         currentWavelength = wavelength;
-        changeImage();
-    } else {
-        // Go to the last available image if current image doesn't exist
+    } else if (imgFilePaths[wavelength]) {
         currentWavelength = wavelength;
-        updateCanvasImage(imgFilePaths[currentWavelength][imgFilePaths[currentWavelength].length - 1]);
-        formatDate(imgFilePaths[currentWavelength][imgFilePaths[currentWavelength].length - 1]);
+        imageIndex = imgFilePaths[wavelength].length - 1;
     }
+    changeImage();
 }
 
 function moveBackImage(multiplier) {
-    if (imageIndex + (imageToSkip * multiplier) <= imgFilePaths[currentWavelength].length - 1) {
-        imageIndex += (imageToSkip * multiplier);
-    } else {
-        imageIndex = imgFilePaths[currentWavelength].length - 1;
-    }
+    imageIndex = Math.min(imageIndex + imageToSkip * multiplier, imgFilePaths[currentWavelength].length - 1);
     changeImage();
 }
 
 function moveForwardImage(multiplier) {
-    if (imageIndex - (imageToSkip * multiplier) >= 0) {
-        imageIndex -= (imageToSkip * multiplier);
-    } else {
-        imageIndex = 0;
-    }
+    imageIndex = Math.max(imageIndex - imageToSkip * multiplier, 0);
     changeImage();
 }
-
-setInterval(getFilePaths, imageUpdateDelay);
 
 function resetDisplay() {
     if (resetDisplayIntervalId) {
         clearInterval(resetDisplayIntervalId);
     }
-
     resetDisplayIntervalId = setInterval(() => {
         imageIndex = 0;
         changeImage();
-        console.log('Display Current image reset');
+        console.log('Display reset to current image');
     }, resetDisplayDelay);
 }
 
-
 async function getFilePaths() {
-    const url = "../image-retriever/wavelengths.json";
     try {
-        const response = await fetch(url);
-
+        const response = await fetch("../image-retriever/wavelengths.json");
         if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
+            throw new Error(`Failed to fetch file paths: ${response.status}`);
         }
-
         imgFilePaths = await response.json();
         console.log('Updated Image Paths');
     } catch (error) {
@@ -142,12 +155,17 @@ async function getFilePaths() {
 }
 
 function formatDate(inputString) {
-    const dateString = inputString.split('-')[1] + '-' + inputString.split('-')[2] + '-' + inputString.split('-')[3].split('T')[0];
+    const parts = inputString.split('-');
+    const dateString = `${parts[1]}-${parts[2]}-${parts[3].split('T')[0]}`;
     const date = new Date(dateString + 'T00:00:00Z');
-
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
+    const monthNames = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
     const formattedDate = `${monthNames[date.getUTCMonth()]} ${date.getUTCDate()}, ${date.getUTCFullYear()}`;
-
     dateText.innerHTML = formattedDate;
 }
+
+setInterval(getFilePaths, imageUpdateDelay);
+
+main();
